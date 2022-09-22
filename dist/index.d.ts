b7f71a574f9efa4095f5a9a6ed76fd30b5f365ce
TuongNguyen14 <tuong14041997@gmail.com>
2022-09-22 22:35:11 +0700

    added index

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export * from "./errors/validation-error";
+export * from "./libraries/array";
+export * from "./libraries/codec";
+export * from "./libraries/object";
+export * from "./libraries/random";
